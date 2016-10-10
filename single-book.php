d2d72dcb02d1a671cc436b29fc72ba043c7903aa
midnightschildren <amar@codisattva.com>
2016-10-10 14:20:55 -0700

    Single Book - pt 1
    change fields, except links…

--- single-book.php
+++ single-book.php
@@ -14,65 +14,65 @@ get_header(); ?>
     $postID = $post->ID; ?>
 	
 		
-		<h1 class="book_title"> <?php print_custom_field('book_title'); ?></h1>
+		<h1 class="book_title"> <?php the_field('book_title'); ?></h1>
         
         <?php // sub title
-          $subTitle = get_custom_field('sub_title');
+          $subTitle = get_field('sub_title');
           if( !empty($subTitle) ) :
         ?>
-		<h1 class="sub_title"> <?php print_custom_field('sub_title'); ?></h1>
+		<h1 class="sub_title"> <?php the_field('sub_title'); ?></h1>
         <?php endif; ?>
         
         <?php // book author
-          $authorTitle = get_custom_field('book_author');
+          $authorTitle = get_field('book_author');
           if( !empty($authorTitle) ) :
         ?>
-		<h2 class="author_title"><?php print_custom_field('book_author'); ?></h2>
+		<h2 class="author_title"><?php the_field('book_author'); ?></h2>
         <?php endif; ?>
         
-		<h3 class="contributor_title"> <?php print_custom_field('book_contributors'); ?></h3>
+		<h3 class="contributor_title"> <?php the_field('book_contributors'); ?></h3>
 </div>
 
 <div class="grid_4 alpha single_book">
         <?php // add to cart
-          $carturl = get_custom_field('cart_url');
+          $carturl = get_field('cart_url');
           if( !empty($carturl) ) :
         ?>
-		<a href="<?php print_custom_field('cart_url'); ?>"><div class="add_to_cart"></div></a>
+		<a href="<?php the_field('cart_url'); ?>"><div class="add_to_cart"></div></a>
         <?php endif; ?>
         <?php // price
-          $bprice = get_custom_field('book_price');
+          $bprice = get_field('book_price');
           if( !empty($bprice) ) :
         ?>        
-    <h3 class="price left-sidebar"><?php print_custom_field('book_price'); ?></h3>
+    <h3 class="price left-sidebar"><?php the_field('book_price'); ?></h3>
         <?php endif; ?>      
 		
         
-		<div class="footnote left-sidebar"><?php echo apply_filters('the_content', get_custom_field('bookdetails')); ?></div>
+		<div class="footnote left-sidebar"><?php echo apply_filters('the_content', get_field('bookdetails')); ?></div>
         
         <?php // Limited Edition
-          $limited = get_custom_field('limited_edition_link:to_link_href');
+          $limited = get_field('limited_edition_link:to_link_href');
           if( !empty($limited) ) :
         ?>
         
-		<p class="led left-sidebar"><a href="<?php print_custom_field('limited_edition_link:to_link_href','http://yoursite.com/default/page/');?>">There's also a Limited Edition</a></p>
+		<p class="led left-sidebar"><a href="<?php the_field('limited_edition_link:to_link_href','http://yoursite.com/default/page/');?>">There's also a Limited Edition</a></p>
         
         <?php endif; ?>
 
         <?php // Blurb box
-          $featuredblurb = get_custom_field('bookfeaturedblurb');
+          $featuredblurb = get_field('bookfeaturedblurb');
           if( !empty($featuredblurb) ) :
         ?>        
         <div class="book_blurb_box">
-		<p class="book_blurb left-sidebar"><?php echo apply_filters('the_content', get_custom_field('bookfeaturedblurb')); ?></p>
+		<p class="book_blurb left-sidebar"><?php echo apply_filters('the_content', get_field('bookfeaturedblurb')); ?></p>
         </div>
         <?php endif; ?>
 
         <?php // Book Links
-          $blinks = get_custom_field('booklinks');
+          $blinks = get_field('booklinks');
           if( !empty($blinks) ) :
         ?>         
-		<div class="book_links"><?php echo apply_filters('the_content', get_custom_field('booklinks')); ?></div>
+		<div class="book_links"><?php echo apply_filters('the_content', get_field('booklinks')); ?></div>
         <?php endif; ?>
 </div>         
 
@@ -80,8 +80,8 @@ get_header(); ?>
 		<?php the_content(); ?>
         
 		<?php // pdf press release
-          $pdfpressrelease = get_custom_field('pdf_press_release');
-          $image_id = get_custom_field('pdf_press_release:raw');
+          $pdfpressrelease = get_field('pdf_press_release');
+          $image_id = get_field('pdf_press_release:raw');
           if( !empty($pdfpressrelease) ) :
 		?>
 		<div class="pdf-press-release">
@@ -89,7 +89,7 @@ get_header(); ?>
 		</div>
 		<?php endif; ?>
 
-		<?php echo apply_filters('the_content', get_custom_field('bookcontent')); ?>
+		<?php echo apply_filters('the_content', get_field('bookcontent')); ?>
 
 <div class="related-posts grid_8 alpha">
       
